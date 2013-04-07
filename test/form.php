218b7540dc3d348414441dbcfb78d5ea8f48836f
vitmalina <vitmalina@gmail.com>
2013-04-06 19:32:16 -0700

    Added tests for grid and form

---
+++ test/form.php
@@ -0,0 +1,32 @@
+<?
+if ($_REQUEST['cmd'] == 'get-record') {
+	print('
+	{
+		"status": "error1",
+		"message": "who cares",
+		"record": {
+			"field_date" 	: "2013-3-15",
+            "field_list"	: "0",
+            "field_list0" 	: "Adams, John",
+            "field_text"	: "1",
+            "field_alpha"	: "2",
+            "field_int"		: "3",
+            "field_float"	: "4",
+            "field_enum"	: [
+            	{ id: 1, text: "Adams, John" },
+            	{ id: "Cruz, Steve", text: "Cruz, Steve" },
+            	{ id: "Lewis, Frank", text: "Lewis, Frank" }
+            ]
+		}
+	}
+	');
+	die();
+}
+
+$ret = Array();
+$ret['status'] 	= 'error1';
+$ret['message'] = 'Unknown error';
+$ret['data'] 	= $_REQUEST;
+
+echo json_encode($ret);
+?>
