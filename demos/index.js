db7e7beb802554b77baf04cbc4873fba216f7f40
vitmalina <vitmalina@gmail.com>
2013-04-16 10:13:54 -0700

    Cleaned up grid examples

--- demos/index.js
+++ demos/index.js
@@ -3,17 +3,30 @@ $(function () {
 	var main_layout = $('#main_layout').w2layout({
 		name: 'main_layout',
 		panels: [
-			{ type: 'left', size: 220, resizable: true, style: 'background-color: #F5F6F7; border-right: 1px solid silver;' },
-			{ type: 'main', style: '' }
+			{ type: 'top', size: 35, style: 'background-color: #323947; color: #C0C8D5;', hidden: true  },
+			{ type: 'left', size: 240, resizable: true, style: 'border-right: 1px solid silver;' },
+			{ type: 'main', style: 'background-color: white;' }
 		]
 	});
+	w2ui['main_layout'].content('top', '<div style="padding: 9px;">'+
+		'Theme: '+
+		'<select onchange="$(\'#mainCSS\').attr(\'href\', this.value);">'+
+		'	<option value="../css/w2ui.min.css">Default Theme</option>'+
+		'	<option value="../css/w2ui-dark.min.css">Dark Theme</option>'+
+		'</select>&nbsp;&nbsp;&nbsp;'+
+		'Locale: '+
+		'<select onchange="w2utils.locale({ path: \'../\', lang: this.value }); alert(\'Localization is only internal functions. You need to refresh to see it.\')">'+
+		'	<option value="en-us">en-US</option>'+
+		'	<option value="fr-fr">fr-FR</option>'+
+		'	<option value="ru-ru">ru-RU</option>'+
+		'</select>'+
+		'</div>');
 	// init sidebar
 	w2ui['main_layout'].content('left', $().w2sidebar({
 		name: 'demo-sidebar',
 		img: null,
-		style: 'padding: 2px;',
 		nodes: [ 
-			{ id: 'layout', text: 'Layout', img: 'icon-folder', group: true,
+			{ id: 'layout', text: 'Layout', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'layout-1', text: 'Simple Layout', icon: 'fa-columns' },
 					{ id: 'layout-2', text: 'Resizable Panels', icon: 'fa-columns' },
@@ -21,10 +34,12 @@ $(function () {
 					{ id: 'layout-4', text: 'Load Content', icon: 'fa-columns' },
 					{ id: 'layout-5', text: 'Transitions', icon: 'fa-columns' },
 					{ id: 'layout-6', text: 'Event Listeners', icon: 'fa-columns' },
-					{ id: 'layout-7', text: 'Nested Layouts', icon: 'fa-columns' }
+					{ id: 'layout-7', text: 'Nested Layouts', icon: 'fa-columns' },
+					{ id: 'layout-8', text: 'Panel With Tabs', icon: 'fa-columns' },
+					{ id: 'layout-9', text: 'Panel With Toolbar', icon: 'fa-columns' }
 				]
 			},
-			{ id: 'grid', text: 'Grid', img: 'icon-folder', group: true,
+			{ id: 'grid', text: 'Grid', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'grid-1', text: 'Simple Grid', icon: 'fa-table' },
 					{ id: 'grid-2', text: 'Record Row Formating', icon: 'fa-table' },
@@ -52,7 +67,7 @@ $(function () {
 					//{ id: 'grid-24', text: 'Locked Columns', icon: 'fa-table' }
 				]
 			},
-			{ id: 'toolbar', text: 'Toolbar', img: 'icon-folder', group: true,
+			{ id: 'toolbar', text: 'Toolbar', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'toolbar-1', text: 'Simple Toolbar', icon: 'fa-hand-up' },
 					{ id: 'toolbar-2', text: 'Advanced Toolbar', icon: 'fa-hand-up' },
@@ -61,7 +76,7 @@ $(function () {
 					{ id: 'toolbar-5', text: 'Enable/Disable Buttons', icon: 'fa-hand-up' }
 				]
 			},
-			{ id: 'sidebar', text: 'Sidebar', img: 'icon-folder', group: true,
+			{ id: 'sidebar', text: 'Sidebar', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'sidebar-1', text: 'Simple Sidebar', icon: 'fa-hand-left' },
 					{ id: 'sidebar-2', text: 'Add/Remove', icon: 'fa-hand-left' },
@@ -69,10 +84,11 @@ $(function () {
 					{ id: 'sidebar-4', text: 'Enable/Disable', icon: 'fa-hand-left' },
 					{ id: 'sidebar-5', text: 'Expand/Collapse', icon: 'fa-hand-left' },
 					{ id: 'sidebar-6', text: 'Select/Unselect', icon: 'fa-hand-left' },
+					{ id: 'sidebar-8', text: 'Top & Bottom HTML', icon: 'fa-hand-left' },
 					{ id: 'sidebar-7', text: 'Events', icon: 'fa-hand-left' }
 				]
 			},
-			{ id: 'tabs', text: 'Tabs', img: 'icon-folder', group: true,
+			{ id: 'tabs', text: 'Tabs', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'tabs-1', text: 'Simple Tabs', icon: 'fa-folder-close-alt' },
 					{ id: 'tabs-2', text: 'Set a Tab Active', icon: 'fa-folder-close-alt' },
@@ -82,18 +98,19 @@ $(function () {
 					{ id: 'tabs-6', text: 'Show/Hide Tabs', icon: 'fa-folder-close-alt' }
 				]
 			},
-			{ id: 'forms', text: 'Forms', img: 'icon-folder', group: true,
+			{ id: 'forms', text: 'Forms', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'forms-1', text: 'Simple Form', icon: 'fa-edit' },
 					{ id: 'forms-2', text: 'Field Types', icon: 'fa-edit' },
 					{ id: 'forms-3', text: 'Large Form', icon: 'fa-edit' },
 					{ id: 'forms-4', text: 'Multi Page Form', icon: 'fa-edit' },
 					{ id: 'forms-5', text: 'Form with Tabs', icon: 'fa-edit' },
+					{ id: 'forms-8', text: 'Form in a Popup', icon: 'fa-edit' },
 					{ id: 'forms-6', text: 'Events', icon: 'fa-edit' },
 					{ id: 'forms-7', text: 'Input Controls', icon: 'fa-edit' }
 				]
 			},
-			{ id: 'popup', text: 'Popup', img: 'icon-folder', group: true,
+			{ id: 'popup', text: 'Popup', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'popup-1', text: 'Simple Popup', icon: 'fa-list-alt' },
 					{ id: 'popup-2', text: 'More Options', icon: 'fa-list-alt' },
@@ -104,7 +121,7 @@ $(function () {
 					{ id: 'popup-7', text: 'Slide a Message', icon: 'fa-list-alt' }
 				]
 			},
-			{ id: 'utils', text: 'Utilities', img: 'icon-folder', group: true,
+			{ id: 'utils', text: 'Utilities', img: 'icon-folder', group1: true,
 				nodes: [
 					{ id: 'utils-1', text: 'Validation', icon: 'fa-star-empty' },
 					{ id: 'utils-2', text: 'Encoding', icon: 'fa-star-empty' },
@@ -209,42 +226,42 @@ $(function () {
 
 		default:
 		case '#!layout':
-			w2ui['demo-sidebar'].doOpen('layout');
+			w2ui['demo-sidebar'].doExpand('layout');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'layout-1');
 			break;
 
 		case '#!grid':
-			w2ui['demo-sidebar'].doOpen('grid');
+			w2ui['demo-sidebar'].doExpand('grid');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'grid-1');
 			break;
 
 		case '#!toolbar':
-			w2ui['demo-sidebar'].doOpen('toolbar');
+			w2ui['demo-sidebar'].doExpand('toolbar');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'toolbar-1');
 			break;
 
 		case '#!sidebar':
-			w2ui['demo-sidebar'].doOpen('sidebar');
+			w2ui['demo-sidebar'].doExpand('sidebar');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'sidebar-1');
 			break;
 
 		case '#!tabs':
-			w2ui['demo-sidebar'].doOpen('tabs');
+			w2ui['demo-sidebar'].doExpand('tabs');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'tabs-1');
 			break;
 
 		case '#!popup':
-			w2ui['demo-sidebar'].doOpen('popup');
+			w2ui['demo-sidebar'].doExpand('popup');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'popup-1');
 			break;
 
 		case '#!forms':
-			w2ui['demo-sidebar'].doOpen('forms');
+			w2ui['demo-sidebar'].doExpand('forms');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'forms-1');
 			break;
 
 		case '#!utils':
-			w2ui['demo-sidebar'].doOpen('utils');
+			w2ui['demo-sidebar'].doExpand('utils');
 			w2ui['demo-sidebar'].doClick(tmp[1] || 'utils-1');
 			break;
 	};
