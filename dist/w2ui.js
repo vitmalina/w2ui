d6b0097f2ecc3e1eec9bcb244991714b47f0b3bf
vitmalina <vitmalina@gmail.com>
2013-11-04 21:49:26 -0800

    Recompiled JS and CSS

--- dist/w2ui.js
+++ dist/w2ui.js
@@ -1,4 +1,4 @@
-/* w2ui 1.3 (c) http://w2ui.com, vitmalina@gmail.com */
+/* w2ui 1.4.x (nightly) (c) http://w2ui.com, vitmalina@gmail.com */
 var w2ui  = w2ui  || {};
 var w2obj = w2obj || {}; // expose object to be able to overwrite default functions
 
@@ -1196,6 +1196,7 @@ w2utils.keyboard = (function (obj) {
 *	- be able to attach events in advanced search dialog
 * 	- reorder columns/records
 *	- hidden searches could not be clearned by the user
+*	- search-logic -> searchLogic
 *
 ************************************************************************/
 
@@ -4197,10 +4198,10 @@ w2utils.keyboard = (function (obj) {
 				// ------ Toolbar Generic buttons
 
 				if (this.show.toolbarReload) {
-					this.toolbar.items.push(this.buttons['reload']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['reload']));
 				}
 				if (this.show.toolbarColumns) {			
-					this.toolbar.items.push(this.buttons['columns']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['columns']));
 					this.initColumnOnOff();
 				}
 				if (this.show.toolbarReload || this.show.toolbarColumn) {
@@ -4228,26 +4229,26 @@ w2utils.keyboard = (function (obj) {
 						'</div>';
 					this.toolbar.items.push({ type: 'html', id: 'search', html: html });
 					if (this.multiSearch && this.searches.length > 0) {
-						this.toolbar.items.push(this.buttons['search-go']);
+						this.toolbar.items.push($.extend(true, {}, this.buttons['search-go']));
 					}
 				}
 				if (this.show.toolbarSearch && (this.show.toolbarAdd || this.show.toolbarEdit || this.show.toolbarDelete || this.show.toolbarSave)) {
 					this.toolbar.items.push({ type: 'break', id: 'break1' });
 				}
 				if (this.show.toolbarAdd) {
-					this.toolbar.items.push(this.buttons['add']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['add']));
 				}
 				if (this.show.toolbarEdit) {
-					this.toolbar.items.push(this.buttons['edit']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['edit']));
 				}
 				if (this.show.toolbarDelete) {
-					this.toolbar.items.push(this.buttons['delete']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['delete']));
 				}
 				if (this.show.toolbarSave) {
 					if (this.show.toolbarAdd || this.show.toolbarDelete || this.show.toolbarEdit) {
 						this.toolbar.items.push({ type: 'break', id: 'break2' });
 					}
-					this.toolbar.items.push(this.buttons['save']);
+					this.toolbar.items.push($.extend(true, {}, this.buttons['save']));
 				}
 				// add original buttons
 				for (var i in tmp_items) this.toolbar.items.push(tmp_items[i]);
@@ -5236,7 +5237,7 @@ w2utils.keyboard = (function (obj) {
 			var col  	= this.columns[col_ind];
 			var record 	= (summary !== true ? this.records[ind] : this.summary[ind]);
 			var data 	= this.parseField(record, col.field);
-			var isChanged = record.changed && record.changes[col.field];
+			var isChanged = record.changed && typeof record.changes[col.field] != 'undefined';
 			if (isChanged) data = record.changes[col.field];
 			// various renderers
 			if (data == null || typeof data == 'undefined') data = '';
