48ed1565bf9c51f03f9ecf9778a0a0ef5708fd8e
vitmalina <vitmalina@gmail.com>
2013-11-17 09:46:57 -0800

    Recompiled JS and CSS

--- dist/w2ui.js
+++ dist/w2ui.js
@@ -1,4 +1,4 @@
-/* w2ui 1.3 (c) http://w2ui.com, vitmalina@gmail.com */
+/* w2ui 1.3.1(working) (c) http://w2ui.com, vitmalina@gmail.com */
 var w2ui  = w2ui  || {};
 var w2obj = w2obj || {}; // expose object to be able to overwrite default functions
 
