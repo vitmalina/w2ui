6bdf215d1969b4b726e5c84ef62e6529ea0be622
Karpenko Yuri <karpenko_yuri@rambler.ru>
2013-11-30 21:12:19 +0400

    Listview: Icons support, ctrl/shift key selection support, perfomance optimization

--- src/w2listview.js
+++ src/w2listview.js
@@ -6,24 +6,22 @@
 *	- Dependencies: jQuery, w2utils
 *
 * == NICE TO HAVE ==
-*	- icons/images support
-*	- "Ctrl"/"Shift" keys support in multiselect mode
-*
-* == 1.4 changes
-*	- deleted getSelection().removeAllRanges() - see https://github.com/vitmalina/w2ui/issues/323
+*	- images support
+*	- PgUp/PgDown keys support
 *
 ************************************************************************/
 
 (function () {
 	var w2listview = function (options) {
-		this.box		= null;		// DOM Element that holds the element
-		this.name		= null;		// unique name for w2ui
-		this.vType		= null;
-		this.items		= [];
-		this.menu		= [];
+		this.box			= null;		// DOM Element that holds the element
+		this.name			= null;		// unique name for w2ui
+		this.vType			= null;
+		this.items			= [];
+		this.menu			= [];
 		this.multiselect	= true;		// multiselect support
 		this.keyboard		= true;		// keyboard support
-		this.focused		= null;		// focused item
+		this.curFocused		= null;		// currently focused item
+		this.selStart		= null;		// item to start selection from (used in selection with "shift" key)
 		this.onClick		= null;
 		this.onDblClick		= null;
 		this.onKeydown		= null;
@@ -40,29 +38,35 @@
 	// -- Registers as a jQuery plugin
 
 	$.fn.w2listview = function(method) {
+		var obj;
 		if (typeof method === 'object' || !method ) {
 			// check name parameter
-			if (!$.fn.w2checkNameParam(method, 'w2listview')) return;
+			if (!$.fn.w2checkNameParam(method, 'w2listview')) return undefined;
 			if (typeof method.viewType !== 'undefined') {
 				method.vType = method.viewType;
 				delete method.viewType;
 			}
-			var items  = method.items;
-			var object = new w2listview(method);
-			$.extend(object, { items: [], handlers: [] });
-			for (var i in items) { object.items[i] = $.extend({}, w2listview.prototype.item, items[i]); }
+			var itms  = method.items;
+			obj = new w2listview(method);
+			$.extend(obj, { items: [], handlers: [] });
+			if ($.isArray(itms)) {
+				for (var i = 0; i < itms.length; i++) {
+					obj.items[i] = $.extend({}, w2listview.prototype.item, itms[i]);
+				}
+			}
 			if ($(this).length !== 0) {
-				object.render($(this)[0]);
+				obj.render($(this)[0]);
 			}
 			// register new object
-			w2ui[object.name] = object;
-			return object;
+			w2ui[obj.name] = obj;
+			return obj;
 		} else if (w2ui[$(this).attr('name')]) {
-			var obj = w2ui[$(this).attr('name')];
+			obj = w2ui[$(this).attr('name')];
 			obj[method].apply(obj, Array.prototype.slice.call(arguments, 1));
 			return this;
 		} else {
 			console.log('ERROR: Method ' +  method + ' does not exist on jQuery.w2listview' );
+			return undefined;
 		}
 	};
 
@@ -71,15 +75,16 @@
 
 	w2listview.prototype = {
 		item : {
-			id		: null,		// param to be sent to all event handlers
-			caption		: '',
-			description	: '',
-			selected	: false,
-			onClick		: null,
-			onDblClick	: null,
-			onKeydown	: null,
+			id				: null,		// param to be sent to all event handlers
+			caption			: '',
+			description		: '',
+			icon			: null,
+			selected		: false,
+			onClick			: null,
+			onDblClick		: null,
+			onKeydown		: null,
 			onContextMenu	: null,
-			onRefresh	: null
+			onRefresh		: null
 		},
 
 		viewType: function (value) {
@@ -111,29 +116,31 @@
 		insert: function (id, item) {
 			if (!$.isArray(item)) item = [item];
 			// assume it is array
-			for (var r in item) {
+			for (var r = 0; r < item.length; r++) {
 				// checks
 				if (String(item[r].id) == 'undefined') {
 					console.log('ERROR: The parameter "id" is required but not supplied. (obj: '+ this.name +')');
 					return;
 				}
 				var unique = true;
-				for (var i in this.items) { if (this.items[i].id == item[r].id) { unique = false; break; } }
+				for (var i = 0; i < this.items.length; i++) {
+					if (this.items[i].id == item[r].id) { unique = false; break; }
+				}
 				if (!unique) {
 					console.log('ERROR: The parameter "id='+ item[r].id +'" is not unique within the current items. (obj: '+ this.name +')');
 					return;
 				}
-				if (!w2utils.isAlphaNumeric(item[r].id)) {
-					console.log('ERROR: The parameter "id='+ item[r].id +'" must be alpha-numeric + "-_". (obj: '+ this.name +')');
-					return;
-				}
+				//if (!w2utils.isAlphaNumeric(item[r].id)) {
+				//	console.log('ERROR: The parameter "id='+ item[r].id +'" must be alpha-numeric + "-_". (obj: '+ this.name +')');
+				//	return;
+				//}
 				// add item
-				item = $.extend({}, item, item[r]);
+				var newItm = $.extend({}, w2listview.prototype.item, item[r]);
 				if (id === null || typeof id == 'undefined') {
-					this.items.push(item);
+					this.items.push(newItm);
 				} else {
 					var middle = this.get(id, true);
-					this.items = this.items.slice(0, middle).concat([item], this.items.slice(middle));
+					this.items = this.items.slice(0, middle).concat([newItm], this.items.slice(middle));
 				}
 				this.refresh(item[r].id);
 			}
@@ -142,21 +149,21 @@
 		remove: function (id) {
 			var removed = 0;
 			for (var i = 0; i < arguments.length; i++) {
-				var item = this.get(arguments[i]);
-				if (!item) return false;
+				var idx = this.get(arguments[i], true);
+				if (idx === null) return false;
 				removed++;
 				// remove from array
-				this.items.splice(this.get(item.id, true), 1);
+				this.items.splice(idx, 1);
 				// remove from screen
-				$(this.box).find('#itm_'+ w2utils.escapeId(item.id)).remove();
+				$(this.box).find('#itm_'+ w2utils.escapeId(arguments[i])).remove();
 			}
 			return removed;
 		},
 
 		set: function (id, item) {
-			var index = this.get(id, true);
-			if (index === null) return false;
-			$.extend(this.items[index], item);
+			var idx = this.get(id, true);
+			if (idx === null) return false;
+			$.extend(this.items[idx], item);
 			this.refresh(id);
 			return true;
 		},
@@ -177,84 +184,155 @@
 		},
 
 		select: function (id, addSelection) {
+			var itm = this.get(id);
+			if (itm === null) return false;
 			if (arguments.length === 1 || !this.multiselect) addSelection = false;
-			if (!addSelection) {
-				for (var i1 in this.items) {
-					if (this.items[i1].id != id) this.unselect(this.items[i1].id);
-				}
-			}
-			var item = this.get(id);
-			if (!item) return false;
-			if (!item.selected) {
-				$(this.box).find('#itm_'+ w2utils.escapeId(id))
+
+			if (!addSelection) this.unselect();
+			if (!itm.selected) {
+				$(this.box)
+					.find('#itm_'+ w2utils.escapeId(id))
 					.addClass('w2ui-selected');
-				item.selected = true;
+				itm.selected = true;
 			}
-			this.focused = id;
-			return item.selected;
+			return itm.selected;
 		},
 
 		unselect: function (id) {
-			for (var i = 0; i < arguments.length; i++) {
-				var item = this.get(arguments[i]);
-				if (item && item.selected) {
-					item.selected = false;
-					$(this.box)
-						.find('#itm_'+ w2utils.escapeId(item.id))
-						.removeClass('w2ui-selected');
-					if (this.focused === item.id) this.focused = null;
-				}
+			var obj = this;
+			var i = 0;
+			if (arguments.length === 0) {
+				for (; i < this.items.length; i++) doUnselect(this.items[i]);
+			} else {
+				for (; i < arguments.length; i++) doUnselect(this.get(arguments[i]));
 			}
 			return true;
+
+			function doUnselect(itm) {
+				if (itm !== null && itm.selected) {
+					itm.selected = false;
+					$(obj.box)
+						.find('#itm_'+ w2utils.escapeId(itm.id))
+						.removeClass('w2ui-selected');
+				}
+			}
+		},
+
+		getFocused: function (returnIndex) {
+			var rslt = this.get(this.curFocused, returnIndex);
+			if (rslt === null) rslt = this.get(this.selStart, returnIndex);
+			return rslt;
+		},
+
+		scrollIntoView: function (id) {
+			if (typeof id != 'undefined') {
+				var itm = this.get(id);
+				if (itm === null) return;
+				var body	= $(this.box);
+				var node	= $(this.box).find('#itm_'+ w2utils.escapeId(id));
+				var offset	= node.offset().top - body.offset().top;
+				var nodeHeight = w2utils.getSize(node, 'height');
+				if (offset + nodeHeight > body.height()) {
+					body.scrollTop( body.scrollTop() + offset + nodeHeight - body.height() );
+				}
+				if (offset <= 0) {
+					body.scrollTop( body.scrollTop() + offset);
+				}
+			}
+		},
+
+		userSelect: function (id, event, isMouse) {
+			var itm = null;
+
+			// update selection
+			if (event.shiftKey) {
+				this.unselect();
+				var fIdx = this.get(this.selStart, true);
+				if (fIdx !== null) {
+					var idx = this.get(id, true);
+					var toIdx = Math.max(idx, fIdx);
+					for (var i = Math.min(idx, fIdx); i <= toIdx; i++) {
+						this.select(this.items[i].id, true);
+					}
+				} else {
+					this.select(id, true);
+					this.selStart = id;
+				}
+			} else if (event.ctrlKey) {
+				if (isMouse) {
+					itm = this.get(id);
+					if (itm.selected) this.unselect(id); else this.select(id, true);
+					this.selStart = id;
+				}
+			} else {
+				this.select(id, false);
+				this.selStart = id;
+			}
+
+			// update focus
+			if (itm === null) itm = this.get(id);
+			if (itm === null) return;
+			var oldItm = this.getFocused();
+			if (oldItm !== null) {
+				$(this.box)
+					.find('#itm_'+ w2utils.escapeId(oldItm.id))
+					.removeClass('w2ui-focused');
+			}
+			$(this.box)
+				.find('#itm_'+ w2utils.escapeId(id))
+				.addClass('w2ui-focused');
+			this.curFocused = id;
+
+			// update view
+			this.scrollIntoView(id);
 		},
 
 		// ===================================================
 		// -- Internal Event Handlers
 
 		click: function (id, event) {
-			var obj = this;
-			var item = obj.get(id);
-			if (item === null) return false;
-			var eventData = obj.trigger({ phase: 'before', type: 'click', target: id, originalEvent: event, object: item });
+			var idx = this.get(id, true);
+			if (idx === null) return false;
+			var eventData = this.trigger({ phase: 'before', type: 'click', target: id, originalEvent: event, object: this.items[idx] });
 			var rslt = eventData.isCancelled !== true;
 			if (rslt) {
 				// default action
-				obj.select(id);
+				this.userSelect(id, event, true);
 				// event after
-				obj.trigger($.extend(eventData, { phase: 'after' }));
+				this.trigger($.extend(eventData, { phase: 'after' }));
 			}
 			return rslt;
 		},
 
 		dblClick: function (id, event) {
-			var obj = this;
-			var item = obj.get(id);
-			if (item === null) return false;
-			var eventData = obj.trigger({ phase: 'before', type: 'dblClick', target: id, originalEvent: event, object: item });
+			var itm = this.get(id);
+			if (itm === null) return false;
+			var eventData = this.trigger({ phase: 'before', type: 'dblClick', target: id, originalEvent: event, object: itm });
 			var rslt = eventData.isCancelled !== true;
 			if (rslt) {
 				// default action
 				// -- empty
 				// event after
-				obj.trigger($.extend(eventData, { phase: 'after' }));
+				this.trigger($.extend(eventData, { phase: 'after' }));
 			}
 			return rslt;
 		},
 
 		keydown: function (event) {
 			var obj = this;
-			var item = this.get(obj.focused);
-			if (item === null || obj.keyboard !== true) return false;
+			var idx = this.getFocused(true);
+			if (idx === null || obj.keyboard !== true) return false;
 			var eventData = obj.trigger({ phase: 'before', type: 'keydown', target: obj.name, originalEvent: event });
 			var rslt = eventData.isCancelled !== true;
 			if (rslt) {
 				// default behaviour
-				if (event.keyCode == 37) selectNeighbor('left');
-				if (event.keyCode == 39) selectNeighbor('right');
-				if (event.keyCode == 38) selectNeighbor('up');
-				if (event.keyCode == 40) selectNeighbor('down');
+				if (event.keyCode == 32) obj.click(obj.items[idx].id, event);
+				if (event.keyCode == 37) processNeighbor('left');
+				if (event.keyCode == 39) processNeighbor('right');
+				if (event.keyCode == 38) processNeighbor('up');
+				if (event.keyCode == 40) processNeighbor('down');
 				// cancel event if needed
-				if ($.inArray(event.keyCode, [37, 38, 39, 40]) != -1) {
+				if ($.inArray(event.keyCode, [32, 37, 38, 39, 40]) != -1) {
 					if (event.preventDefault) event.preventDefault();
 					if (event.stopPropagation) event.stopPropagation();
 				}
@@ -264,28 +342,29 @@
 			}
 			return rslt;
 
-			function selectNeighbor(neighbor) {
-				var idx = obj.get(item.id, true);
+			function processNeighbor(neighbor) {
 				var newIdx;
 				if (neighbor === 'up') newIdx = idx - itemsInLine();
 				if (neighbor === 'down') newIdx = idx + itemsInLine();
 				if (neighbor === 'left') newIdx = idx - 1;
 				if (neighbor === 'right') newIdx = idx + 1;
-				if (newIdx >= 0 && newIdx < obj.items.length && newIdx != idx) obj.select(obj.items[newIdx].id);
+				if (newIdx >= 0 && newIdx < obj.items.length && newIdx != idx) {
+					obj.userSelect(obj.items[newIdx].id, event, false);
+				}
 			}
 
 			function itemsInLine() {
 				var lv = $(obj.box).find('> ul');
-				return ~~(lv.width() / w2utils.getSize(lv.find('> li').get(0), 'width'));
+				return parseInt(lv.width() / w2utils.getSize(lv.find('> li').get(0), 'width'), 10);
 			}
 		},
 
 		contextMenu: function (id, event) {
 			var obj = this;
-			var item = this.get(id);
-			if (item === null) return false;
-			if (!item.selected) obj.select(id);
-			var eventData = obj.trigger({ phase: 'before', type: 'contextMenu', target: id, originalEvent: event, object: item });
+			var itm = this.get(id);
+			if (itm === null) return false;
+			if (!itm.selected) obj.select(id);
+			var eventData = obj.trigger({ phase: 'before', type: 'contextMenu', target: id, originalEvent: event, object: itm });
 			var rslt = eventData.isCancelled !== true;
 			if (rslt) {
 				// default action
@@ -304,59 +383,69 @@
 		},
 
 		menuClick: function (itemId, index, event) {
-			var obj = this;
 			// event before
-			var eventData = obj.trigger({ phase: 'before', type: 'menuClick', target: itemId, originalEvent: event, menuIndex: index, menuItem: obj.menu[index] });
+			var eventData = this.trigger({ phase: 'before', type: 'menuClick', target: itemId, originalEvent: event, menuIndex: index, menuItem: this.menu[index] });
 			var rslt = eventData.isCancelled !== true;
 			if (rslt) {
 				// default action
 				// -- empty
 				// event after
-				obj.trigger($.extend(eventData, { phase: 'after' }));
+				this.trigger($.extend(eventData, { phase: 'after' }));
 			}
 			return rslt;
 		},
 
-		refresh: function (id) {
-			var time = (new Date()).getTime();
-			// if (window.getSelection) window.getSelection().removeAllRanges(); // clear selection
-			if (String(id) == 'undefined') {
-				// refresh all
-				for (var i in this.items) this.refresh(this.items[i].id);
-			}
-			// event before
-			var eventData = this.trigger({ phase: 'before', type: 'refresh', target: (typeof id != 'undefined' ? id : this.name), object: this.get(id) });
-			if (eventData.isCancelled === true) return false;
-			// create or refresh only one item
-			var item = this.get(id);
-			if (item === null) return false;
-			//if (typeof item.caption != 'undefined') item.text = item.caption;
-
-			var jq_el   = $(this.box).find('#itm_'+ w2utils.escapeId(item.id));
-			var itemHTML =
-					'<div class="icon-small">small icon</div> ' +
-					'<div class="icon-medium">medium icon</div> ' +
-					'<div class="icon-large">large icon</div> ' +
+		getItemHTML: function (item, onlyInner) {
+			var iconClass = (item.icon !== null && typeof item.icon != 'undefined') ? ' '+item.icon : ' icon-none';
+			var innerHTML =
+					'<div class="icon-small' + iconClass + '"></div> ' +
+					'<div class="icon-medium' + iconClass + '"></div> ' +
+					'<div class="icon-large' + iconClass + '"></div> ' +
 					'<div class="caption">' + item.caption + '</div> ' +
 					'<div class="description">' + item.description + '</div>';
-			if (jq_el.length === 0) {
-				// does not exist - create it
-				html =	'<li id="itm_'+ item.id +'" ' +
+			if (onlyInner) {
+				return innerHTML;
+			} else {
+				return '<li id="itm_'+ item.id +'" ' +
 					'onclick="w2ui[\''+ this.name +'\'].click(\''+ item.id +'\', event);" '+'' +
 					'ondblclick="w2ui[\''+ this.name +'\'].dblClick(\''+ item.id +'\', event);" '+
 					'oncontextmenu="w2ui[\''+ this.name +'\'].contextMenu(\''+ item.id +'\', event); if (event.preventDefault) event.preventDefault();" '+
-					'>'+ itemHTML + '</li>';
-				if (this.get(id, true) != this.items.length-1 && $(this.box).find('#itm_'+ w2utils.escapeId(this.items[parseInt(this.get(id, true))+1].id)).length > 0) {
-					$(this.box).find('#itm_'+ w2utils.escapeId(this.items[parseInt(this.get(id, true))+1].id)).before(html);
-				} else {
-					$(this.box).find('#itmlast').before(html);
-				}
-			} else {
-				// refresh
-				jq_el.html(itemHTML);
+					'>'+ innerHTML + '</li>';
 			}
-			// event after
-			this.trigger($.extend(eventData, { phase: 'after' }));
+		},
+
+		refresh: function (id) {
+			var time = (new Date()).getTime();
+			if (String(id) == 'undefined') {
+				// refresh all items
+				this.render(this.box);
+			} else {
+				// create or refresh only one item
+
+				// event before
+				var eventData = this.trigger({ phase: 'before', type: 'refresh', target: (typeof id != 'undefined' ? id : this.name), object: this.get(id) });
+				if (eventData.isCancelled === true) return false;
+
+				var idx = this.get(id, true);
+				if (idx === null) return false;
+				var jq_el = $(this.box).find('#itm_'+ w2utils.escapeId(id));
+				if (jq_el.length === 0) {
+					// does not exist - create it
+					var nextItm;
+					if (idx != this.items.length-1) {
+						nextItm = $(this.box).find('#itm_'+ w2utils.escapeId(this.items[idx+1].id));
+						if (nextItm.length === 0) nextItm = undefined;
+					}
+					if (!nextItm) nextItm = $(this.box).find('#itmlast');
+					nextItm.before(this.getItemHTML(this.items[idx], false));
+				} else {
+					// refresh
+					jq_el.html(this.getItemHTML(this.items[idx], true));
+				}
+				// event after
+				this.trigger($.extend(eventData, { phase: 'after' }));
+			}
+
 			return (new Date()).getTime() - time;
 		},
 
@@ -366,8 +455,7 @@
 			var eventData = this.trigger({ phase: 'before', type: 'render', target: this.name, box: box });
 			if (eventData.isCancelled === true) return false;
 			// default action
-			// if (window.getSelection) window.getSelection().removeAllRanges(); // clear selection
-			if (String(box) != 'undefined' && box !== null) {
+			if (String(box) != 'undefined' && box !== null && this.box !== box) {
 				if ($(this.box).find('> ul #itmlast').length > 0) {
 					$(this.box)
 						.removeAttr('name')
@@ -378,14 +466,15 @@
 			}
 			if (!this.box) return false;
 			// render all items
-			var html =	'<ul><li id="itmlast" style="display: none;"></li></ul>';
+			var html = '<ul>';
+			for (var i = 0; i < this.items.length; i++) html += this.getItemHTML(this.items[i], false);
+			html += '<li id="itmlast" style="display: none;"></li></ul>';
 			$(this.box)
 				.attr('name', this.name)
 				.addClass('w2ui-reset w2ui-listview w2ui-' + this.viewType())
 				.html(html);
 			// event after
 			this.trigger($.extend(eventData, { phase: 'after' }));
-			this.refresh();
 			return (new Date()).getTime() - time;
 		},
 
@@ -403,6 +492,7 @@
 			delete w2ui[this.name];
 			// event after
 			this.trigger($.extend(eventData, { phase: 'after' }));
+			return true;
 		}
 	};
 
