83633632aab86911babd411739499cae409aa516
Vitali Malinouski <vitmalina@gmail.com>
2014-07-06 20:07:20 -0700

    Updated documentation for w2form

--- docs/index.js
+++ docs/index.js
@@ -34,11 +34,11 @@ $(function () {
                 { id: 'w2sidebar-props', text: 'Properties', icon: 'fa-star-empty' },
                 { id: 'w2sidebar-methods', text: 'Methods', icon: 'fa-cog' }
             ] },
-            { id: 'w2listview', text: 'w2listview', img: 'icon-folder', group1: true, nodes: [
-                { id: 'w2listview-events', text: 'Events', icon: 'fa-tag' },
-                { id: 'w2listview-props', text: 'Properties', icon: 'fa-star-empty' },
-                { id: 'w2listview-methods', text: 'Methods', icon: 'fa-cog' }
-            ] },
+            // { id: 'w2listview', text: 'w2listview', img: 'icon-folder', group1: true, nodes: [
+            //     { id: 'w2listview-events', text: 'Events', icon: 'fa-tag' },
+            //     { id: 'w2listview-props', text: 'Properties', icon: 'fa-star-empty' },
+            //     { id: 'w2listview-methods', text: 'Methods', icon: 'fa-cog' }
+            // ] },
             { id: 'w2tabs', text: 'w2tabs', img: 'icon-folder', group1: true, nodes: [
                 { id: 'w2tabs-events', text: 'Events', icon: 'fa-tag' },
                 { id: 'w2tabs-props', text: 'Properties', icon: 'fa-star-empty' },
@@ -71,7 +71,7 @@ $(function () {
     $().w2grid({ name: 'test-grid' });
     $().w2sidebar({ name: 'test-sidebar' });
     $().w2toolbar({ name: 'test-toolbar' });
-    $().w2listview({ name: 'test-listview' });
+    // $().w2listview({ name: 'test-listview' });
     $().w2tabs({ name: 'test-tabs' });
     $().w2form({ name: 'test-form' });
 
@@ -80,7 +80,7 @@ $(function () {
     init('grid');
     init('sidebar');
     init('toolbar');
-    init('listview');
+    // init('listview');
     init('tabs');
     init('form');
     initPopup();
@@ -108,7 +108,7 @@ $(function () {
         'w2grid.isIOS',
         'w2toolbar.item',
         'w2sidebar.node',
-        'w2listview.item', 'w2listview.vType', 'w2listview.itemExtra', 'w2listview.itemNode', 'w2listview.itemNodeId', 'w2listview.itemNodeOffsetInfo',
+        // 'w2listview.item', 'w2listview.vType', 'w2listview.itemExtra', 'w2listview.itemNode', 'w2listview.itemNodeId', 'w2listview.itemNodeOffsetInfo',
         'w2tabs.tab'
     );
 
