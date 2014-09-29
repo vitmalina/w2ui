be7b09fe1b35d0239b87170e706f59859402a9bf
Vitali Malinouski <vitmalina@gmail.com>
2014-09-29 00:47:06 -0700

    Fixed bug in list/enum fields

--- src/w2fields.js
+++ src/w2fields.js
@@ -1401,7 +1401,7 @@
                 for (var i = 0; i < options.items.length; i++) {
                     var item = options.items[i];
                     if (typeof options.compare == 'function') {
-                        item.hidden = (options.compare.call(this, item) === false ? true : false)
+                        item.hidden = (options.compare.call(this, item, search) === false ? true : false)
                     } else {
                         var prefix = '';
                         var suffix = '';
