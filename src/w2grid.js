3cba1607dc812a945b769a5b7fc61d1467207cf2
Vitali Malinouski <vitmalina@gmail.com>
2015-02-28 21:37:22 -0800

    Fixed #823

--- src/w2grid.js
+++ src/w2grid.js
@@ -656,7 +656,7 @@
                                 break;
                             case 'ends':
                             case 'ends with': // need for back compatib.
-                                if (val1.indexOf(val2) == val1.length - val2.length) fl++; // do not hide record
+                                if (val1.indexOf(val2) >= 0 && val1.indexOf(val2) == val1.length - val2.length) fl++; // do not hide record
                                 break;
                         }
                     }
