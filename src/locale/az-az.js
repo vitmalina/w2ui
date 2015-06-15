48c49aa3976b7143a1b69f6c039a2f56e807059b
Adil Aliyev <adilaliev@gmail.com>
2015-06-16 01:25:20 +0500

    Create az-az.js

---
+++ src/locale/az-az.js
@@ -0,0 +1,17 @@
+{
+    "locale"         : "az-AZ",
+    "date_format"    : "dd/mm/yyyy",
+    "time_format"    : "h24",
+    "currency"       : "^[\\$]?[-+]?[0-9]*[\\.]?[0-9]+$",
+    "currencyPrefix" : "",
+    "currencySuffix" : "AZN",
+    "groupSymbol"    : ",",
+    "float"          : "^[-]?[0-9]*[\\.]?[0-9]+$",
+    "shortmonths"    : ["Yan", "Fev", "Mar", "Apr", "May", "İyun", "İyul", "Avq", "Sen", "Okt", "Noy", "Dek"],
+    "fullmonths"     : ["Yanvar", "Fevral", "Mart", "Aprel", "May", "İyun", "İyul", "Avqust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"],
+    "shortdays"      : ["BE", "ÇA", "Ç", "CA", "C", "Ş","B"],
+    "fulldays"       : ["Bazar ertəsi", "Çərşənbə axşamı", "Çərşənbə", "Cümə axşamı", "Cümə", "Şənbə", "Bazar"],
+    "phrases" : {
+
+    }
+}
