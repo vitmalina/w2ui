2b8751dbd91125eaec5008cd9bdb2df619365817
aadrian <a.adrian.tech@googlemail.com>
2015-06-23 13:09:09 +0200

    change some urls to correctly reference the bower downloaded dependencies.

--- docs/code-mirror.js
+++ docs/code-mirror.js
@@ -5,10 +5,10 @@ $(function () {
     $('head').append(
         '<!-- CODE MIRROR -->\n'+
         '<link rel="stylesheet" href="code-mirror.css">\n'+
-        '<script src="../libs/CodeMirror/lib/codemirror.js"></script>\n'+
-        '<script src="../libs/CodeMirror/mode/javascript/javascript.js"></script>\n'+
-        '<script src="../libs/CodeMirror/mode/htmlmixed/htmlmixed.js"></script>\n'+
-        '<script src="../libs/CodeMirror/mode/css/css.js"></script>\n'+
-        '<script src="../libs/CodeMirror/mode/xml/xml.js"></script>\n'
+        '<script src="../libs/codemirror/lib/codemirror.js"></script>\n'+
+        '<script src="../libs/codemirror/mode/javascript/javascript.js"></script>\n'+
+        '<script src="../libs/codemirror/mode/htmlmixed/htmlmixed.js"></script>\n'+
+        '<script src="../libs/codemirror/mode/css/css.js"></script>\n'+
+        '<script src="../libs/codemirror/mode/xml/xml.js"></script>\n'
     );
 });
