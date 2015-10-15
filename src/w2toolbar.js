61bf098de672d57caa5df0d91b7044f232666427
Gilad Novik <gilad@novik.ca>
2015-10-15 22:13:11 +0300

    fix for removal of first item in a toolbar

--- src/w2toolbar.js
+++ src/w2toolbar.js
@@ -143,7 +143,7 @@
                 $(this.box).find('#tb_'+ this.name +'_item_'+ w2utils.escapeId(it.id)).remove();
                 // remove from array
                 var ind = this.get(it.id, true);
-                if (ind != null) this.items.splice(ind, 1);
+                if (ind >= 0) this.items.splice(ind, 1);
             }
             return removed;
         },
