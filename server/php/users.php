3252c014f3a5cc1140e066c4450fa471c97a738c
Diego Mendoza <mendozada@gmail.com>
2016-11-03 10:23:09 -0300

    Update users.php
    The so-called get-record does not exist in the dist w2ui.js

--- server/php/users.php
+++ server/php/users.php
@@ -9,10 +9,18 @@ $db->connect("127.0.0.1", "root", "", "test", "3306");
 switch ($_REQUEST['cmd']) {
 
     case 'get':
-        $sql  = "SELECT * FROM users
-                 WHERE ~search~ ORDER BY ~sort~";
-        $res = $w2grid->getRecords($sql, null, $_REQUEST);
-        $w2grid->outputJSON($res);
+        if (array_key_exists('recid', $_REQUEST)){  // if true , then is a 'get-record' only one record with recid
+            $sql = "SELECT userid, fname, lname, email, login, password
+                    FROM users
+                    WHERE userid = ".$_REQUEST['recid'];
+            $res = $w2grid->getRecord($sql);
+        }
+        else{        
+            $sql  = "SELECT * FROM users       
+                     WHERE ~search~ ORDER BY ~sort~";
+            $res = $w2grid->getRecords($sql, null, $_REQUEST);
+        }  
+         $w2grid->outputJSON($res);
         break;
 
     case 'delete':
@@ -20,13 +28,8 @@ switch ($_REQUEST['cmd']) {
         $w2grid->outputJSON($res);
         break;
 
-    case 'get-record':
-        $sql = "SELECT userid, fname, lname, email, login, password
-                FROM users
-                WHERE userid = ".$_REQUEST['recid'];
-        $res = $w2grid->getRecord($sql);
-        $w2grid->outputJSON($res);
-        break;
+    //case 'get-record':
+    //    break;
 
     case 'save':
         $res = $w2grid->saveRecord('users', 'userid', $_REQUEST);
