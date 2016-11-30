a0edfe1e8a7c4877b6bb81945313721aa4a5703e
MaximPetrichuk <petrichuk@gmail.com>
2016-11-30 10:39:17 +0600

    Create w2chart-methods.php

---
+++ docs/summary/w2chart-methods.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="../summary.css"/> 
+<div class="container">
+<div class="obj-property">
+    <a href="w2chart.add">add</a> <span>- add(series)</span>
+</div>
+<div class="obj-property-desc">
+    Adds a series to the chart.
+</div>
+
+</div>
