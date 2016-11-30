262b14e6cef4200f7f503e1f6c63eb81e83b6ae0
MaximPetrichuk <petrichuk@gmail.com>
2016-11-30 10:27:18 +0600

    Create w2chart-props.php

---
+++ docs/summary/w2chart-props.php
@@ -0,0 +1,60 @@
+
+<link rel="stylesheet" type="text/css" href="../summary.css"/> 
+<div class="container">
+<div class="obj-property">
+    <a href="w2chart.header">header</a> <span>- String, default = ''</span>
+</div>
+<div class="obj-property-desc">
+    The header of the chart. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.margin">margin</a> <span>- Object, default - see below</span>
+</div>
+<div class="obj-property-desc">
+    Padding from edges of the chart. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.xAxis">xAxis</a> <span>- Object, default - see below</span>
+</div>
+<div class="obj-property-desc">
+    Map of parameter x axis. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.yLeftAxis">yLeftAxis</a> <span>- Object, default - see below</span>
+</div>
+<div class="obj-property-desc">
+    Map of parameter left y axis. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.yRightAxis">yRightAxis</a> <span>- Object, default - see below</span>
+</div>
+<div class="obj-property-desc">
+    Map of parameter right y axis. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.series">series</a> <span>- Array, default = []</span>
+</div>
+<div class="obj-property-desc">
+    Array of series objects. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.records">records</a> <span>- Array, default = []</span>
+</div>
+<div class="obj-property-desc">
+    Array of record objects. 
+</div>
+
+<div class="obj-property">
+    <a href="w2chart.show">show</a> <span>- Object, default - see below</span>
+</div>
+<div class="obj-property-desc">
+    Map of indicators which elements of the chart are visible.
+</div>
+
+</div>
