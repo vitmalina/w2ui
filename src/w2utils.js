82bccb1c91043e84c6d82982f3148cb5fe064e11
Steve Mansour <sman@stevemansour.com>
2017-02-03 12:31:44 -0800

    FIX: regexp now allows for a dash in the local part of the email address. So email addresses like ys-k37@outlook.com pass validation

--- src/w2utils.js
+++ src/w2utils.js
@@ -152,7 +152,7 @@ var w2utils = (function ($) {
     }
 
     function isEmail (val) {
-        var email = /^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        var email = /^[-a-zA-Z0-9._%-+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         return email.test(val);
     }
 
