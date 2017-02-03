fe9a4e0e92ae14ed3663a9d87744a2e885982fff
Constantin Groß <cg@48design.de>
2017-02-03 17:49:02 +0100

    check -> check-square-o

--- site/pages/demo/demo-sidebar.php
+++ site/pages/demo/demo-sidebar.php
@@ -83,7 +83,7 @@ w2ui['layout'].content('left', $().w2sidebar({
 		{ id: 'level-1', text: 'Level 1', img: 'icon-folder', expanded: true, group: true,
 		  nodes: [ { id: 'level-1-1', text: 'Level 1.1', icon: 'fa fa-home' },
 				   { id: 'level-1-2', text: 'Level 1.2', icon: 'fa fa-star' },
-				   { id: 'level-1-3', text: 'Level 1.3', icon: 'fa fa-check' }
+				   { id: 'level-1-3', text: 'Level 1.3', icon: 'fa fa-check-square-o' }
 				 ]
 		},
 		{ id: 'level-2', text: 'Level 2', img: 'icon-folder', expanded: true, group: true,
@@ -134,7 +134,7 @@ $(function () {
 			{ id: 'level-1', text: 'Level 1', img: 'icon-folder', expanded: true, group: true,
 			  nodes: [ { id: 'level-1-1', text: 'Level 1.1', icon: 'fa fa-home' },
 					   { id: 'level-1-2', text: 'Level 1.2', icon: 'fa fa-star' },
-					   { id: 'level-1-3', text: 'Level 1.3', icon: 'fa fa-check' }
+					   { id: 'level-1-3', text: 'Level 1.3', icon: 'fa fa-check-square-o' }
 					 ]
 			},
 			{ id: 'level-2', text: 'Level 2', img: 'icon-folder', expanded: true, group: true,
