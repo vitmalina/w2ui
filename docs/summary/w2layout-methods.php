4ada9c5ea64d72ada25732293d39505c0a6b5e9d
Ruslan Makarsky <mrg2001@gmail.com>
2017-02-11 09:06:59 -0800

    Updated website summary pages

--- docs/summary/w2layout-methods.php
+++ docs/summary/w2layout-methods.php
@@ -1,140 +1,139 @@
-<link rel="stylesheet" type="text/css" href="../summary.css"/> 
 <div class="container">
 <div class="obj-property">
-    <a href="w2layout.assignToolbar">assignToolbar</a> <span>- assignToolbar(panel, toolbar)</span>
+    <a href="../w2layout.assignToolbar">assignToolbar</a> <span>- assignToolbar(panel, toolbar)</span>
 </div>
 <div class="obj-property-desc">
     Assigned new w2toolbar object to a panel
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.content">content</a> <span>- content(type, [content], [transition])</span>
+    <a href="../w2layout.content">content</a> <span>- content(type, [content], [transition])</span>
 </div>
 <div class="obj-property-desc">
     Sets or retrieves panel content.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.el">el</a> <span>- el(type)</span>
+    <a href="../w2layout.el">el</a> <span>- el(type)</span>
 </div>
 <div class="obj-property-desc">
     Returns DOM element where content of the panel is rendered.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.get">get</a> <span>- get(type)</span>
+    <a href="../w2layout.get">get</a> <span>- get(type)</span>
 </div>
 <div class="obj-property-desc">
     Finds the panel and returns its object. 
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.hide">hide</a> <span>- hide(panel, [immediate])</span>
+    <a href="../w2layout.hide">hide</a> <span>- hide(panel, [immediate])</span>
 </div>
 <div class="obj-property-desc">
     Hides the panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.hideTabs">hideTabs</a> <span>- hideTabs(type)</span>
+    <a href="../w2layout.hideTabs">hideTabs</a> <span>- hideTabs(type)</span>
 </div>
 <div class="obj-property-desc">
     Hides the panel's tabs.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.hideToolbar">hideToolbar</a> <span>- hideToolbar(type)</span>
+    <a href="../w2layout.hideToolbar">hideToolbar</a> <span>- hideToolbar(type)</span>
 </div>
 <div class="obj-property-desc">
     Hides the panel's toolbar.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.html">html</a> <span>- html(type, [content], [transition])</span>
+    <a href="../w2layout.html">html</a> <span>- html(type, [content], [transition])</span>
 </div>
 <div class="obj-property-desc">
     Sets or retrieves panel content.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.load">load</a> <span>- load(type, url, [transition], [onLoad])</span>
+    <a href="../w2layout.load">load</a> <span>- load(type, url, [transition], [onLoad])</span>
 </div>
 <div class="obj-property-desc">
     Loads content and renders it inside the panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.lock">lock</a> <span>- lock(panel, message, [showSpinner])</span>
+    <a href="../w2layout.lock">lock</a> <span>- lock(panel, message, [showSpinner])</span>
 </div>
 <div class="obj-property-desc">
     Locks layout panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.message">message</a> <span>- message(panel, msgOptions);</span>
+    <a href="../w2layout.message">message</a> <span>- message(panel, msgOptions);</span>
 </div>
 <div class="obj-property-desc">
     Displays a message at the top of panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.set">set</a> <span>- set(type, panel)</span>
+    <a href="../w2layout.set">set</a> <span>- set(type, panel)</span>
 </div>
 <div class="obj-property-desc">
     Finds the panel and extends it with provided object. 
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.show">show</a> <span>- show(panel, [immediate])</span>
+    <a href="../w2layout.show">show</a> <span>- show(panel, [immediate])</span>
 </div>
 <div class="obj-property-desc">
     Shows the panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.showTabs">showTabs</a> <span>- showTabs(type)</span>
+    <a href="../w2layout.showTabs">showTabs</a> <span>- showTabs(type)</span>
 </div>
 <div class="obj-property-desc">
     Shows the panel's tabs.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.showToolbar">showToolbar</a> <span>- showToolbar(type)</span>
+    <a href="../w2layout.showToolbar">showToolbar</a> <span>- showToolbar(type)</span>
 </div>
 <div class="obj-property-desc">
     Shows the panel's toolbar.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.sizeTo">sizeTo</a> <span>- sizeTo(type, size, [instant])</span>
+    <a href="../w2layout.sizeTo">sizeTo</a> <span>- sizeTo(type, size, [instant])</span>
 </div>
 <div class="obj-property-desc">
     Resizes the panel with animation
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.toggle">toggle</a> <span>- toggle(type, [immediate])</span>
+    <a href="../w2layout.toggle">toggle</a> <span>- toggle(type, [immediate])</span>
 </div>
 <div class="obj-property-desc">
     Toggles the panel.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.toggleTabs">toggleTabs</a> <span>- toggleTabs(type)</span>
+    <a href="../w2layout.toggleTabs">toggleTabs</a> <span>- toggleTabs(type)</span>
 </div>
 <div class="obj-property-desc">
     Toggles the panel's tabs.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.toggleToolbar">toggleToolbar</a> <span>- toggleToolbar(type)</span>
+    <a href="../w2layout.toggleToolbar">toggleToolbar</a> <span>- toggleToolbar(type)</span>
 </div>
 <div class="obj-property-desc">
     Toggles the panel's toolbar.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.unlock">unlock</a> <span>- unlock(panel)</span>
+    <a href="../w2layout.unlock">unlock</a> <span>- unlock(panel)</span>
 </div>
 <div class="obj-property-desc">
     Unlocks layout panel.
--- docs/summary/w2layout-props.php
+++ docs/summary/w2layout-props.php
@@ -1,28 +1,27 @@
-<link rel="stylesheet" type="text/css" href="../summary.css"/> 
 <div class="container">
 <div class="obj-property">
-    <a href="w2layout.padding">padding</a> <span>- Integer, default = 1</span>
+    <a href="../w2layout.padding">padding</a> <span>- Integer, default = 1</span>
 </div>
 <div class="obj-property-desc">
     Padding between panels in px.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.panels">panels</a> <span>- Array, default = []</span>
+    <a href="../w2layout.panels">panels</a> <span>- Array, default = []</span>
 </div>
 <div class="obj-property-desc">
     Array of panel objects.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.resizer">resizer</a> <span>- Integer, default = 4</span>
+    <a href="../w2layout.resizer">resizer</a> <span>- Integer, default = 4</span>
 </div>
 <div class="obj-property-desc">
     The size of draggable resizer between panels.
 </div>
 
 <div class="obj-property">
-    <a href="w2layout.tmp">tmp</a> <span>- Object, default = {}</span>
+    <a href="../w2layout.tmp">tmp</a> <span>- Object, default = {}</span>
 </div>
 <div class="obj-property-desc">
     Object with temporary variables for internal use
