9593056d523b473a7d75be98eb416ff5bbfc763e
Szirmai László (Otthoni) <it@szirtech.hu>
2017-02-12 23:58:18 +0100

    radio grid control

--- dist/w2ui.js
+++ dist/w2ui.js
@@ -10278,14 +10278,26 @@ w2utils.event = {
                 }
             } else {
                 // if editable checkbox
-                if (edit && ['checkbox', 'check', 'radio'].indexOf(edit.type) != -1) {
+                if (edit && ['checkbox', 'check'].indexOf(edit.type) != -1) {
                     var changeInd = summary ? -(ind + 1) : ind;
                     style += 'text-align: center;';
-                    data = '<input tabindex="-1" type="'+edit.type+'" '+ (data ? 'checked="checked"' : '') +' onclick="' +
+                    data = '<input tabindex="-1" type="checkbox" '+ (data ? 'checked="checked"' : '') +' onclick="' +
                            '    var obj = w2ui[\''+ this.name + '\']; '+
                            '    obj.editChange.call(obj, this, '+ changeInd +', '+ col_ind +', event); ' +
                            '"/>';
                     infoBubble = '';
+                } else if (edit && edit.items && edit.type == 'radio') {
+                    var changeInd = summary ? -(ind + 1) : ind;
+                    style += 'text-align: center;';
+                    var val = data;
+                    data='';
+                    for(var i=0;i<edit.items.length;i++) {
+                        data += '<input tabindex="-1" type="radio" '+ (val==edit.items[i] ? 'checked="checked"' : '') +' value="'+edit.items[i]+'" onclick="' +
+                                '    var obj = w2ui[\''+ this.name + '\']; '+
+                                '    obj.editChange.call(obj, this, '+ changeInd +', '+ col_ind +', event); ' +
+                                '"/> '+edit.items[i]+' ';
+                    }
+                    infoBubble = '';                        
                 }
                 if (this.show.recordTitles) {
                     // title overwrite
