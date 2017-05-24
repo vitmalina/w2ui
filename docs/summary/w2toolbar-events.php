bcdd5cc1beec1f4faae1f28fd0b02edee759e59b
Ernesto Domato <edomato@gmail.com>
2017-05-24 10:32:20 -0300

    Fixed some documentation paths

--- docs/summary/w2toolbar-events.php
+++ docs/summary/w2toolbar-events.php
@@ -1,6 +1,6 @@
 <div class="container">
 <div class="obj-property">
-    <a href="../w2toolbar.onClick">onClick</a> <span>- function (event)</span>
+    <a href="w2toolbar.onClick">onClick</a> <span>- function (event)</span>
 </div>
 <div class="obj-property-desc">
     Called when toolbar item is clicked.
