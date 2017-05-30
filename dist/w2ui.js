bb465e5881fbc42023e20e60821585918ed7f06f
Szirmai László (Otthoni) <it@szirtech.hu>
2017-05-30 16:40:29 +0200

    zero currencyPrecision bugfix

--- dist/w2ui.js
+++ dist/w2ui.js
@@ -1823,7 +1823,7 @@ w2utils.formatters = {
 
     'money': function (value, params) {
         if (value == null || value === '') return '';
-        var data = w2utils.formatNumber(Number(value), w2utils.settings.currencyPrecision || 2);
+        var data = w2utils.formatNumber(Number(value), w2utils.settings.currencyPrecision===0?0:w2utils.settings.currencyPrecision || 2 );
         return (w2utils.settings.currencyPrefix || '') + data + (w2utils.settings.currencySuffix || '');
     },
 
