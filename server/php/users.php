38352ba5064731b99beb518ec2bfd158d698d681
Diego Mendoza <mendozada@gmail.com>
2018-05-14 14:32:20 -0300

    Update users.php
    Takes the request as a string and transforms it into an array, this way this example works better because $ _request had a problem with the 'cmd'. 
    
    regards.

--- server/php/users.php
+++ server/php/users.php
@@ -6,25 +6,30 @@ require("w2lib.php");
 $db = new dbConnection("mysql");
 $db->connect("127.0.0.1", "root", "", "test", "3306");
 
-switch ($_REQUEST['cmd']) {
+$jsonTexto = $_POST['request'];       //takes the request as a string and transforms it into an array
+$decodedTexto = html_entity_decode($jsonTexto);
+$miArray = json_decode($decodedTexto, true);
+
+
+switch ($miArray['cmd']) {
 
     case 'get':
-        if (array_key_exists('recid', $_REQUEST)){  // if true , then is a 'get-record' only one record with recid
+        if (array_key_exists('recid', $miArray)){  // if true , then is a 'get-record' only one record with recid
             $sql = "SELECT userid, fname, lname, email, login, password
                     FROM users
-                    WHERE userid = ".$_REQUEST['recid'];
+                    WHERE userid = ".$miArray['recid'];
             $res = $w2grid->getRecord($sql);
         }
         else{        
             $sql  = "SELECT * FROM users       
                      WHERE ~search~ ORDER BY ~sort~";
-            $res = $w2grid->getRecords($sql, null, $_REQUEST);
+            $res = $w2grid->getRecords($sql, null, $miArray);
         }  
          $w2grid->outputJSON($res);
         break;
 
     case 'delete':
-        $res = $w2grid->deleteRecords("users", "userid", $_REQUEST);
+        $res = $w2grid->deleteRecords("users", "userid", $miArray);
         $w2grid->outputJSON($res);
         break;
 
@@ -32,15 +37,15 @@ switch ($_REQUEST['cmd']) {
     //    break;
 
     case 'save':
-        $res = $w2grid->saveRecord('users', 'userid', $_REQUEST);
+        $res = $w2grid->saveRecord('users', 'userid', $miArray);
         $w2grid->outputJSON($res);
         break;
 
     default:
         $res = Array();
         $res['status']  = 'error';
-        $res['message'] = 'Command "'.$_REQUEST['cmd'].'" is not recognized.';
-        $res['postData']= $_REQUEST;
+        $res['message'] = 'Command "'.$miArray['cmd'].'" is not recognized.';
+        $res['postData']= $miArray;
         $w2grid->outputJSON($res);
         break;
 }
