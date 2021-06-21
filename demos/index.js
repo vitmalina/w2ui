56de0936940bd5d479d08781c718ace3471c402b
Mike <mpf82@users.noreply.github.com>
2021-06-21 16:07:39 +0200

    w2form docs and demo
    w2form docs and demo

--- demos/index.js
+++ demos/index.js
@@ -211,6 +211,7 @@ $(function () {
                         { id: 'form/18', text: 'Context Messages', icon: 'fa fa-pencil-square-o' },
                         { id: 'form/19', text: 'Collapsible Groups', icon: 'fa fa-pencil-square-o' },
                         { id: 'form/20', text: 'Form Actions', icon: 'fa fa-pencil-square-o' },
+                        { id: 'form/23', text: 'Lock/Unlock Form', icon: 'fa fa-pencil-square-o' },
                     ]
                 },
                 // { id: 'forms-2.0', text: 'Features 2.0+', img: 'icon-folder', group: true, expanded: true, hidden: true,
