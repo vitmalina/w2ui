f972fa8980c4ce348db41f2d9b35f0588b897cca
Vitali Malinouski <vitmalina@gmail.com>
2021-10-08 05:14:44 +0200

    Update start.js

--- es6mods/start.js
+++ es6mods/start.js
@@ -1,15 +1,14 @@
 import '../libs/jquery/jquery-3.5.1.js'
-// import '../dist/w2ui.js'
-
+import { w2ui, w2utils, w2tabs, w2toolbar, w2sidebar, w2grid } from '../dist/w2ui.es6.js'
 import '../src/w2compat.js'
-import { w2popup, w2alert, w2confirm, w2prompt } from '../src/w2popup.js'
-import { w2tabs } from '../src/w2tabs.js'
-import { w2toolbar } from '../src/w2toolbar.js'
-import { w2sidebar } from '../src/w2sidebar.js'
-import { w2layout } from '../src/w2layout.js'
-import { w2field, addType, removeType } from '../src/w2field.js'
-import { w2form } from '../src/w2form.js'
-import { w2grid } from '../src/w2grid.js'
+// import { w2popup, w2alert, w2confirm, w2prompt } from '../src/w2popup.js'
+// import { w2tabs } from '../src/w2tabs.js'
+// import { w2toolbar } from '../src/w2toolbar.js'
+// import { w2sidebar } from '../src/w2sidebar.js'
+// import { w2layout } from '../src/w2layout.js'
+// import { w2field, addType, removeType } from '../src/w2field.js'
+// import { w2form } from '../src/w2form.js'
+// import { w2grid } from '../src/w2grid.js'
 
 import conf_tabs from './conf-tabs.js'
 import conf_toolbar from './conf-toolbar.js'
@@ -19,23 +18,23 @@ import conf_form from './conf-form.js'
 import conf_grid from './conf-grid3.js'
 
 // w2field
-addType('myType', function (options) {
-    $(this.el).on('keypress', function (event) {
-        if (event.metaKey || event.ctrlKey || event.altKey
-            || (event.charCode != event.keyCode && event.keyCode > 0)) return;
-        var ch = String.fromCharCode(event.charCode);
-        if (ch != 'a' && ch != 'b' && ch != 'c') {
-            if (event.stopPropagation) event.stopPropagation(); else event.cancelBubble = true;
-            return false;
-        }
-    });
-    $(this.el).on('blur', function (event)  { // keyCode & charCode differ in FireFox
-        var ch = this.value;
-        if (ch != 'a' && ch != 'b' && ch != 'c') {
-            $(this).w2tag(w2utils.lang("Not a single character from the set of 'abc'"), { position: 'top|bottom' });
-        }
-    });
-})
+// addType('myType', function (options) {
+//     $(this.el).on('keypress', function (event) {
+//         if (event.metaKey || event.ctrlKey || event.altKey
+//             || (event.charCode != event.keyCode && event.keyCode > 0)) return;
+//         var ch = String.fromCharCode(event.charCode);
+//         if (ch != 'a' && ch != 'b' && ch != 'c') {
+//             if (event.stopPropagation) event.stopPropagation(); else event.cancelBubble = true;
+//             return false;
+//         }
+//     });
+//     $(this.el).on('blur', function (event)  { // keyCode & charCode differ in FireFox
+//         var ch = this.value;
+//         if (ch != 'a' && ch != 'b' && ch != 'c') {
+//             $(this).w2tag(w2utils.lang("Not a single character from the set of 'abc'"), { position: 'top|bottom' });
+//         }
+//     });
+// })
 // let field = new w2field('myType')
 // field.render($('input.w2field')[0])
 $('input').val('E06666').w2field('color')
