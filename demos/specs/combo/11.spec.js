b168b7f38bd6f53bfc473b1bde6b2df85f25e62c
Vitali Malinouski <vitmalina@gmail.com>
2021-11-05 10:50:04 -0700

    Renamed some tests

--- demos/specs/combo/11.spec.js
+++ demos/specs/combo/11.spec.js
@@ -4,26 +4,27 @@ context("1: Combo", () => {
             bela
                 .ready('/w2ui/demos/#/combo/11')
                 .let('@sb', '#layout_layout_panel_left .w2ui-sidebar')
-                .get('@sb').find('>div')
-                .should({
-                    'be.visible': '',
-                    'not.have.class': 'w2ui-sidebar-flat',
-                    'have.css': {
-                        'width': '200px'
-                    }
-                })
-                .get('@sb').find('.w2ui-flat-left')
-                .click()
-                .wait(300) // time to shrink
-                .get('@sb').find('>div')
-                .should({
-                    'be.visible': true,
-                    'have.class': 'w2ui-sidebar-flat',
-                    'have.css': {
-                        'width': '35px'
-                    }
-                })
+                .begin('Shrink Sidebar')
+                    .get('@sb').find('>div')
+                    .should({
+                        'be.visible': '',
+                        'not.have.class': 'w2ui-sidebar-flat',
+                        'have.css': {
+                            'width': '200px'
+                        }
+                    })
+                    .get('@sb').find('.w2ui-flat-left')
+                    .click()
+                    .wait(300) // time to shrink
+                    .get('@sb').find('>div')
+                    .should({
+                        'be.visible': true,
+                        'have.class': 'w2ui-sidebar-flat',
+                        'have.css': {
+                            'width': '35px'
+                        }
+                    })
+                .end()
         })
-
     })
 })
