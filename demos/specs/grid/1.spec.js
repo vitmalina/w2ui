51527ec39e89a64e0e70090dac5ead1ea595847f
Vitali Malinouski <vitmalina@gmail.com>
2021-11-11 15:14:21 -0600

    Updated grid specs

--- demos/specs/grid/1.spec.js
+++ demos/specs/grid/1.spec.js
@@ -1,11 +1,13 @@
 context("3:Grid", () => {
-    test("1: Simple grid", () => {
-        bela
-            .ready('/w2ui/demos/#/grid/1')
-            .grid('grid')
-            .should('have.records')
-            .should('have.subset', [{ fname: 'John', lname: 'Doe' }])
-            .should('have.subset', [{ fname: 'Sergei', lname: 'Rachmaninov' }])
+    context("0: Basic", () => {
+        test("1: Simple grid", () => {
+            bela
+                .ready('/w2ui/demos/#/grid/1')
+                .grid('grid')
+                .should('have.records')
+                .should('have.subset', [{ fname: 'John', lname: 'Doe' }])
+                .should('have.subset', [{ fname: 'Sergei', lname: 'Rachmaninov' }])
 
+        })
     })
 })
--- demos/specs/grid/23.spec.js
+++ demos/specs/grid/23.spec.js
@@ -1,16 +1,18 @@
 context("3:Grid", () => {
-    test("4:Cell Formatting", () => {
-        bela
-            .ready('/w2ui/demos/#/grid/23')
-            .grid('grid')
-            .should({
-                'have.records': '>4',
-                'have.subset': [{ fname: 'John', lname: 'Doe', profit: 2500 }]
-            })
-            .get('#grid_grid_rec_1')
-            .should({
-                'contain.text': ['John Doe', '$2,500.00'],
-                'not.contain.text': ['2500', 'Stuart']
-            })
+    context("0: Basic", () => {
+        test("4:Cell Formatting", () => {
+            bela
+                .ready('/w2ui/demos/#/grid/23')
+                .grid('grid')
+                .should({
+                    'have.records': '>4',
+                    'have.subset': [{ fname: 'John', lname: 'Doe', profit: 2500 }]
+                })
+                .get('#grid_grid_rec_1')
+                .should({
+                    'contain.text': ['John Doe', '$2,500.00'],
+                    'not.contain.text': ['2500', 'Stuart']
+                })
+        })
     })
 })
