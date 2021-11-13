ffe064d1d10eb76168bf26325616714e59a54441
Vitali Malinouski <vitmalina@gmail.com>
2021-11-12 22:39:45 -0800

    Updated some demos

--- demos/specs/grid/6.spec.js
+++ demos/specs/grid/6.spec.js
@@ -3,23 +3,27 @@ context("3:Grid", () => {
         test("6: Single or Multi Select", () => {
             bela
                 .ready('/w2ui/demos/#/grid/6')
-                .grid('grid')
-                .should('have.records', '>0')
-                .click(0, { metaKey: true, ctrlKey: true })
-                .click(1, { metaKey: true, ctrlKey: true })
-                .should({
-                    'have.records': true,
-                    'have.selection': 1
-                })
-                .get('#singleOrMulti')
-                .click()
-                .grid('grid')
-                .click(0, { metaKey: true, ctrlKey: true })
-                .click(1, { metaKey: true, ctrlKey: true })
-                .should({
-                    'have.records': true,
-                    'have.selection': 2
-                })
+                .begin('Single select')
+                    .grid('grid')
+                    .should('have.records', '>0')
+                    .click(0, { metaKey: true, ctrlKey: true })
+                    .click(1, { metaKey: true, ctrlKey: true })
+                    .should({
+                        'have.records': true,
+                        'have.selection': 1
+                    })
+                .end()
+                .begin('Multi select')
+                    .get('#singleOrMulti')
+                    .click()
+                    .grid('grid')
+                    .click(0, { metaKey: true, ctrlKey: true })
+                    .click(1, { metaKey: true, ctrlKey: true })
+                    .should({
+                        'have.records': true,
+                        'have.selection': 2
+                    })
+                .end()
         })
     })
 })
