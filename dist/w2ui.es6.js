534c197b67c5c6ba5d2525fe7169648019980b8a
Vitali Malinouski <vitmalina@gmail.com>
2025-03-18 15:43:20 -0700

    Recompied JS

--- dist/w2ui.es6.js
+++ dist/w2ui.es6.js
@@ -1,4 +1,4 @@
-/* w2ui 2.0.x (nightly) (3/18/2025, 3:22:11 PM) (c) http://w2ui.com, vitmalina@gmail.com */
+/* w2ui 2.0.x (nightly) (3/18/2025, 3:39:08 PM) (c) http://w2ui.com, vitmalina@gmail.com */
 /**
  * Part of w2ui 2.0 library
  *  - Dependencies: w2utils
@@ -4140,6 +4140,8 @@ let w2popup = new Dialog()
  * - options.prefilter - if true, it will show prefiltered items for w2menu, otherwise all
  * - menu.item.help, menu.item.hotkey, menu.item.extra
  * - options.selected -> for w2menu
+ * - options.tooltip => {}
+ * - w2menu event onTooltip
  */
 
 class Tooltip {
@@ -5677,7 +5679,7 @@ class MenuTooltip extends Tooltip {
                 let dt = event.target.dataset
                 let tooltip = overlay.options.items[dt.index]?.tooltip
                 if (tooltip && dt.hassubmenu != 'yes') {
-                    this.showTooltip(overlay.name, { tooltip })
+                    this.showTooltip(overlay.name, { tooltip, anchor: event.target })
                 }
                 // hide previous sub-menu if any
                 let _menu = query(event.target).closest('.w2ui-menu').get(0)
@@ -5972,17 +5974,18 @@ class MenuTooltip extends Tooltip {
                 })
             }
         }
+        w2tooltip.hide(overlay.name + '-tooltip')
     }
     showTooltip(name, options) {
         let overlay = Tooltip.active[name.replace(/[\s\.#]/g, '_')]
         if (!overlay || !overlay.displayed) return
-        let el = query(overlay.box).find(`.w2ui-menu-item[index="${overlay.selected}"]`).get(0)
+        let anchor = options?.anchor ?? query(overlay.box).find(`.w2ui-menu-item[index="${overlay.selected}"]`).get(0)
         let tooltip = options?.tooltip ?? overlay.options.items[overlay.selected]?.tooltip
         if (tooltip) {
             let html = tooltip.html ?? tooltip
             w2tooltip.show(Object.assign({
                 name: overlay.name + '-tooltip',
-                anchor: el,
+                anchor,
                 html,
                 position: 'right|left',
                 hideOn: ['doc-click'],
