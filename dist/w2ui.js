11d5b71c404e65550557a716e9d51709a1edeb61
Vitali Malinouski <vitmalina@gmail.com>
2025-05-20 17:14:27 -0700

    Recompiled JS

--- dist/w2ui.js
+++ dist/w2ui.js
@@ -1,4 +1,4 @@
-/* w2ui 2.0.x (nightly) (5/20/2025, 4:30:46 PM) (c) http://w2ui.com, vitmalina@gmail.com */
+/* w2ui 2.0.x (nightly) (5/20/2025, 5:14:07 PM) (c) http://w2ui.com, vitmalina@gmail.com */
 /**
  * Part of w2ui 2.0 library
  *  - Dependencies: w2utils
@@ -21044,6 +21044,9 @@ class w2form extends w2base {
         let errors = []
         for (let f = 0; f < this.fields.length; f++) {
             let field = this.fields[f]
+            if (field.type == 'columns' || field.field == null) {
+                continue
+            }
             if (this.getValue(field.field) == null) this.setValue(field.field, '')
             if (['int', 'float', 'currency', 'money'].indexOf(field.type) != -1) {
                 let val = this.getValue(field.field)
@@ -21213,6 +21216,9 @@ class w2form extends w2base {
     getCleanRecord(strict) {
         let data = w2utils.clone(this.record)
         this.fields.forEach((fld) => {
+            if (fld.type == 'columns' || fld.field == null) {
+                return
+            }
             if (['list', 'combo', 'enum'].indexOf(fld.type) != -1) {
                 let tmp = { nestedFields: true, record: data }
                 let val = this.getValue.call(tmp, fld.field)
@@ -21706,7 +21712,7 @@ class w2form extends w2base {
                 group = field.html.group
             }
             if (field.type == 'columns') {
-                html += '<div class="w2ui-field-columns">'
+                html += `<div class="w2ui-field-columns" style="${field.style ?? ''}">`
                 field.columns.forEach(col => {
                     html += `<div style="${col.style}"> ${col.content} </div>`
                 })
